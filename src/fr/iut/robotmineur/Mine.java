5e14ed5a0f1fecfdb22ad6529f019ed910c22422
FABI <fatou10bintousall@gmail.com>
2026-05-13 10:12:09 +0200

    dg

---
+++ src/fr/iut/robotmineur/Mine.java
@@ -0,0 +1,66 @@
+package fr.iut.robotmineur;
+
+public class Mine {
+
+    private int id;
+    private Position position;
+    private TypeMinerai typeMinerai;
+    private int quantiteActuelle;
+    private int quantiteInitiale;
+
+    public Mine(int id, Position position, TypeMinerai typeMinerai,
+                int quantiteActuelle, int quantiteInitiale) {
+
+        this.id = id;
+        this.position = position;
+        this.typeMinerai = typeMinerai;
+        this.quantiteActuelle = quantiteActuelle;
+        this.quantiteInitiale = quantiteInitiale;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public TypeMinerai getTypeMinerai() {
+        return typeMinerai;
+    }
+
+    public int getQuantiteActuelle() {
+        return quantiteActuelle;
+    }
+
+    public int getQuantiteInitiale() {
+        return quantiteInitiale;
+    }
+
+    public boolean estVide() {
+        return quantiteActuelle <= 0;
+    }
+
+    public int extraire(int quantite) {
+
+        if (quantiteActuelle >= quantite) {
+            quantiteActuelle -= quantite;
+            return quantite;
+        }
+
+        int reste = quantiteActuelle;
+        quantiteActuelle = 0;
+
+        return reste;
+    }
+
+    @Override
+    public String toString() {
+        return "M" + id +
+                " " + position +
+                " " + typeMinerai +
+                " " + quantiteActuelle +
+                "/" + quantiteInitiale;
+    }
+}
